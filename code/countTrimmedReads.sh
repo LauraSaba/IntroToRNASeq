cec7abff14182d3c1de48af0c36c7188f28ac937
Laura Saba <Laura.Saba@cuanschutz.edu>
2022-02-24 12:29:15 -0700

    update for CPBS7712

--- code/countTrimmedReads.sh
+++ code/countTrimmedReads.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-FILES1=/projectFolder/trimmedReads/*.fastq
+FILES1=/projectFolder/trimmed_reads/*.fastq
 for f in $FILES1
 do
-	awk '/@GWZHISEQ/ {getline; print length($0)}' $f | awk -v sample="$f" '{sum+=$1} END {print sample,sum/NR,NR}' >> /projectFolder/data/trimmedReadCounts.txt
+	awk '/@SRR/ {getline; print length($0)}' $f | awk -v sample="$f" '{sum+=$1} END {print sample,sum/NR,NR}' >> /projectFolder/data/trimmedReadCounts.txt
 done
--- code/trimReads.sh
+++ code/trimReads.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-FILES1=/projectFolder/rawReads/*R1*.fastq
+FILES1=/projectFolder/raw_reads/*_1*.fastq
 for f in $FILES1
 do
-	f2=${f//R1/R2} 
+	f2=${f//_1/_2} 
 	f_trimmed=${f//.fastq/_trimmed.fastq}
-	f_trimmed=${f_trimmed//rawReads/trimmedReads}
+	f_trimmed=${f_trimmed//raw_reads/trimmed_reads}
 	f2_trimmed=${f2//.fastq/_trimmed.fastq}
-	f2_trimmed=${f2_trimmed//rawReads/trimmedReads}
+	f2_trimmed=${f2_trimmed//raw_reads/trimmed_reads}
 	cutadapt -q 20 -m 20 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCACCCGTCCCGATCTCGTATGCCGTCTTCTGCTTG -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT -o $f_trimmed -p $f2_trimmed $f $f2
 done
-
